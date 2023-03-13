fbd095da41e3a9eb1089678f9a1ddbfc0089b651
Didimukhtar <jamilatmukhtar2014@gmail.com>
2023-03-14 00:48:17 +0100

    redo task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) 
-{
-    console.log(line);
-}
+const texts = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+  ];
+  
+  for (let i = 0; i < texts.length; i++) {
+    console.log(texts[i]);
+  }
+
