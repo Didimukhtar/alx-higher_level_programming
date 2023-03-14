51b1615b147d1e2b1b34ca09b403fb44adba219d
Didimukhtar <jamilatmukhtar2014@gmail.com>
2023-03-14 02:21:17 +0100

    standard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,8 @@ const texts = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
-];
+]
 
 for (let i = 0; i < texts.length; i++) {
-  console.log(texts[i]);
+  console.log(texts[i])
 }
