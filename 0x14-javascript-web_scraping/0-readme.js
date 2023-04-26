3f6308a68ae77424996c71e1613f990b1d15f5d5
Didimukhtar <jamilatmukhtar2014@gmail.com>
2023-04-26 17:00:20 +0100

    edit variable

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const ft = require('ft');
-ft.readFile(process.argv[2], 'utf8', function (error, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
